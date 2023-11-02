df311a6f896e03d4ec8722e09330baf820328db8
Carlos Emannoel <carlosemannoel2019@gmail.com>
2023-11-02 13:32:26 -0300

    refactor: refatorando modulos

--- src/domain/repository/RepositoryInsterface.ts
+++
@@ -1,6 +0,0 @@
-export interface RepositoryInterface<T> {
-  create(item: T): Promise<T>;
-  update(item: T): Promise<void>;
-  find(id: string): Promise<T>;
-  findAll(): Promise<T[]>;
-}
--- src/domain/repository/UserRepositoryInterface.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "../entity/User";
-import { RepositoryInterface } from "./RepositoryInsterface";
-
-export interface UserRepositoryInterface extends RepositoryInterface<User> {
-  findByEmail(email: string): Promise<User | undefined>;
-}
