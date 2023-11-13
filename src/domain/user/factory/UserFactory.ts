e98a62da7871e45bd59891dff52254e39faacdb8
Carlos Emannoel <carlosemannoel2019@gmail.com>
2023-11-13 20:25:08 -0300

    feat: update de usuários e teste

--- src/domain/user/factory/UserFactory.ts
+++ src/domain/user/factory/UserFactory.ts
@@ -1,7 +1,8 @@
 import { User } from "../entity/User";
 import { v4 as UUIDV4 } from "uuid";
+import { InputCreateUserFactory } from "./UserFactoryTypes";
 export class UserFactory {
-  static createNewUser(name: string, email: string, password: string) {
+  static createNewUser({email, name, password}: InputCreateUserFactory) {
     return new User(UUIDV4(), name, email, password, new Date(), new Date());
   }
 }
