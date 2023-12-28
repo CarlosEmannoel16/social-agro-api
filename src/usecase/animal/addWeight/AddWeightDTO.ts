f2962ecc02d0b5ad52537eae7686dc970e4ff524
Carlos Emannoel <carlosemannoel2019@gmail.com>
2023-12-28 08:18:25 -0300

    feat: ajustando funcionalidade de histórico de peso do animal

---
+++ src/usecase/animal/addWeight/AddWeightDTO.ts
@@ -0,0 +1,6 @@
+export type InputAddWeightAnimalDTO = {
+  idAnimal: string;
+  idUser: string;
+  weight: number;
+  date: Date;
+};
---
+++ src/usecase/animal/addWeight/AddWeigthUseCase.ts
@@ -0,0 +1,21 @@
+import { AnimalRepositoryInterface } from "../../../domain/animal/repository/AnimaProtocolRepository";
+import { InputAddWeightAnimalDTO } from "./AddWeightDTO";
+
+export class AddWeightAnimalUseCase {
+  constructor(private readonly animalRepository: AnimalRepositoryInterface) {}
+  async execute(data: InputAddWeightAnimalDTO) {
+    if(!data.idAnimal || !data.idUser || !data.weight || !data.date) throw new Error("Dados inválidos")
+    console.log(data)
+    const animal = await this.animalRepository.find(data.idUser, data.idAnimal);
+    if(!animal) throw new Error("Animal não encontrado")
+    console.log(animal)
+    await this.animalRepository.addWeight({
+      date: new Date(data.date),
+      idAnimal: data.idAnimal,
+      idUser: data.idUser,
+      weight: Number(data.weight),
+    });
+
+    
+  }
+}
