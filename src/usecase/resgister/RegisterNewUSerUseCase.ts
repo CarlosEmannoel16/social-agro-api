03dd7ef89577060e79dad31998d864509cff91fb
Carlos Emannoel <carlosemannoel2019@gmail.com>
2024-07-06 22:52:39 -0300

    feat: add validation in register user

--- src/usecase/resgister/RegisterNewUSerUseCase.ts
+++ src/usecase/resgister/RegisterNewUSerUseCase.ts
@@ -1,3 +1,24 @@
+import { UserRepositoryInterface } from "@/domain/user/repository/UserRepositoryInterface";
+
+export type InputRegisterNewUserUseCase = {
+  email: string;
+  name: string;
+  password: string;
+  phone: string;
+};
 export class RegisterNewUserUseCase {
-  handle() {}
+  constructor(private readonly userRepository: UserRepositoryInterface) {}
+  async handle(params: InputRegisterNewUserUseCase) {
+    const existsEmail = await this.userRepository.checkIfExistsByEmail(
+      params.email
+    );
+
+    if (existsEmail) throw new Error("Email already exists");
+
+    const existsPhone = await this.userRepository.checkIfExistsByPhone(
+      params.phone
+    );
+
+    if (existsPhone) throw new Error("Phone already exists");
+  }
 }
